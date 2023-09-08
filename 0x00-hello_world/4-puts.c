f638b8debab0181e7f99a56a2ab3ac9c85ee1a47
adesola0000 <adesolarachael2018@gmail.com>
2023-09-08 10:04:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - this is the main put function
-* Return: 0
+* main - entry point
+* Return: always 0 (Success)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
