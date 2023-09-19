b587e6e5fdc6fd6465daa5a1eed19f4347ce2321
adesola0000 <142162323+adesola0000@users.noreply.github.com>
2023-09-19 06:10:09 +0100

    Update 0-reset_to_98.c
    reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,18 @@
 #include "main.h"
-
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter
- * and updates the value it points to to 98.
- *
- * @n: input
- * Return: n
+ * main - function
+ * Return: 0
  */
-
-void reset_to_98(int *n)
+int main(void)
 {
-	*n = 98;
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
 }
